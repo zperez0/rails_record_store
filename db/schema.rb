0decb5085f6f84573eaca7d1358d10d8a5ffc68d
Yazmin Perez <zoeperez333@gmail.com>
2022-05-24 15:51:50 -0700

    include songstable

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_24_221846) do
+ActiveRecord::Schema.define(version: 2022_05_24_224838) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,15 @@ ActiveRecord::Schema.define(version: 2022_05_24_221846) do
   create_table "albums", force: :cascade do |t|
     t.string "name"
     t.integer "year"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "genre"
+  end
+
+  create_table "songs", force: :cascade do |t|
+    t.string "name"
+    t.string "lyrics"
+    t.integer "album_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
