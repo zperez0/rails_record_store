aa12cff5e0fcb0abb1e2307ea60edffcac24be5d
Yazmin Perez <zoeperez333@gmail.com>
2022-05-24 15:54:24 -0700

    add foreign_key table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_24_224838) do
+ActiveRecord::Schema.define(version: 2022_05_24_225229) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,4 +31,5 @@ ActiveRecord::Schema.define(version: 2022_05_24_224838) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "songs", "albums"
 end
