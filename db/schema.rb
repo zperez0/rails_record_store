cd41ae88820f8ab147a2e6b1ea4413208db59051
Yazmin Perez <zoeperez333@gmail.com>
2022-05-26 16:31:02 -0700

    update schema to include artists and join table albums_artists

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_24_225229) do
+ActiveRecord::Schema.define(version: 2022_05_26_223227) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,19 @@ ActiveRecord::Schema.define(version: 2022_05_24_225229) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "genre"
+  end
+
+  create_table "albums_artists", id: false, force: :cascade do |t|
+    t.bigint "artist_id"
+    t.bigint "album_id"
+    t.index ["album_id"], name: "index_albums_artists_on_album_id"
+    t.index ["artist_id"], name: "index_albums_artists_on_artist_id"
+  end
+
+  create_table "artists", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "songs", force: :cascade do |t|
