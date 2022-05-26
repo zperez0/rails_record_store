42c76dc986c44aeae74f532bd56770fd7ed6a2a9
Yazmin Perez <zoeperez333@gmail.com>
2022-05-26 12:21:49 -0700

    add validation to albums

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,3 +1,11 @@
 class Album < ApplicationRecord
   has_many :songs, dependent: :destroy
+  validates :name, presence: true
+  validates_length_of :name, maximum: 100
+  before_save(:titleize_album)
+
+  private
+    def titleize_album
+      self.name = self.name.titleize
+    end
 end
