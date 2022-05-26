15bdc340904a865344a76e53b9e97879595e3b05
Yazmin Perez <zoeperez333@gmail.com>
2022-05-26 12:21:10 -0700

    add validation testing for albums

--- spec/models/album_spec.rb
+++ spec/models/album_spec.rb
@@ -2,4 +2,13 @@ require 'rails_helper'
 
 describe Album do
   it { should have_many(:songs) }
+  it { should validate_presence_of :name}
+  it { should validate_length_of(:name).is_at_most(100) }
+end
+
+describe Album do
+  it("titleizes the name of an album") do
+    album = Album.create({name: "giant steps", genre: "jazz"})
+    expect(album.name).to eq "Giant Steps"
+  end
 end
