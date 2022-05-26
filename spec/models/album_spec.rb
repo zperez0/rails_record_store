8ee5c3c9759daccc5fcd80764fd6ebd8a8bd4025
Yazmin Perez <zoeperez333@gmail.com>
2022-05-26 15:39:13 -0700

    add testing for artists

--- spec/models/album_spec.rb
+++ spec/models/album_spec.rb
@@ -1,9 +1,10 @@
 require 'rails_helper'
 
 describe Album do
-  it { should have_many(:songs) }
+  it { should have_many :songs }
   it { should validate_presence_of :name}
   it { should validate_length_of(:name).is_at_most(100) }
+  it { should have_and_belong_to_many :artists }
 end
 
 describe Album do
