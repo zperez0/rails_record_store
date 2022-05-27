9eee6f83069ddc9bb86aee3fd333785dc7bb988f
Yazmin Perez <zoeperez333@gmail.com>
2022-05-26 17:01:09 -0700

    add scope

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,4 +1,7 @@
 class Album < ApplicationRecord
+  scope :rock, -> { where genre: "Rock" } # search function specifically to find rock genre
+  scope :name, -> (name_parameter) { where name: name_parameter } # search function to find whatever album name the user intputs
+  scope :search, -> (name_parameter) { where("name like ?", "%#{name_parameter}%")} # search function to display all of the albums name containing a specific set of letters the user entered // case senstivtive use lower method?
   has_and_belongs_to_many :artists
   has_many :songs, dependent: :destroy
   validates :name, presence: true
